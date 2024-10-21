1d7d9db0a4d51878a7d3a1831b965d7bc64dff7c
Gökhan TIKNAZOGLU <gtiknazoglu@ulugenerji.com.tr>
2024-10-21 16:52:51 +0300

    .

---
+++ src/Humanity.Infrastructure/Migrations/20241021135226_aboneEntity.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Humanity.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class aboneEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Tarife",
+                table: "Abone",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "SozlesmeGucu",
+                table: "Abone",
+                type: "double precision",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "SeriNo",
+                table: "Abone",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "KuruluGuc",
+                table: "Abone",
+                type: "double precision",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EtsoKodu",
+                table: "Abone",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DagitimFirmaId",
+                table: "Abone",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Tarife",
+                table: "Abone",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "SozlesmeGucu",
+                table: "Abone",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "SeriNo",
+                table: "Abone",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "KuruluGuc",
+                table: "Abone",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EtsoKodu",
+                table: "Abone",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DagitimFirmaId",
+                table: "Abone",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+    }
+}
