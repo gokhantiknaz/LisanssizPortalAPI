3e8d73a0f84e55fcb9eb3d311dfca5e45de0a224
Gökhan TIKNAZOGLU <gtiknazoglu@ulugenerji.com.tr>
2024-11-04 15:03:32 +0300

    migration

---
+++ src/Humanity.Infrastructure/Migrations/20241104120319_entegrasyon.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Humanity.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class entegrasyon : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_MusteriEntegrasyon_MusteriId",
+                table: "MusteriEntegrasyon");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MusteriEntegrasyon_MusteriId",
+                table: "MusteriEntegrasyon",
+                column: "MusteriId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_MusteriEntegrasyon_MusteriId",
+                table: "MusteriEntegrasyon");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MusteriEntegrasyon_MusteriId",
+                table: "MusteriEntegrasyon",
+                column: "MusteriId",
+                unique: true);
+        }
+    }
+}
